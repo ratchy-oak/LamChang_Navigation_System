90569f01dcbdb1162b0f11b33aa8a45acf3df4f1
Ratchakrit Mojomsin <rachakritmojomsin@gmail.com>
2024-01-04 21:07:18 +0700

    4/1/2024

--- application/lib/config/config.dart
+++ application/lib/config/config.dart
@@ -1,4 +1,4 @@
-const url = 'http://192.168.0.24:3000/';
+const url = 'http://localhost:3000/';
 const registration = "${url}registration";
 const login = "${url}login";
 const forgetpassword = "${url}forgetpassword";
