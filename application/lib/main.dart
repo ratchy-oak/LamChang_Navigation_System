90f9398f585a76d79496b5886677980d364601fa
Ratchakrit Mojomsin <rachakritmojomsin@gmail.com>
2024-01-06 06:57:36 +0700

    6/1/2024

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,23 +1,9 @@
-import 'package:application/pages/helper_page.dart';
 import 'package:application/pages/login_page.dart';
-import 'package:application/pages/user_page.dart';
 import 'package:flutter/material.dart';
 import 'package:application/styles/app_colors.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  runApp(MyApp(
-    token: prefs.getString('token'),
-  ));
-}
 
 class MyApp extends StatelessWidget {
-  // ignore: prefer_typing_uninitialized_variables
-  final token;
-  const MyApp({super.key, this.token});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +15,6 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
           unselectedWidgetColor: AppColors.yellow,
         ),
-        home: (JwtDecoder.isExpired(token) == false)
-            ? UserPage(token: token)
-            : const LoginPage());
+        home: const LoginPage());
   }
 }
--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -38,7 +38,7 @@ exports.login = async(req, res, next) => {
 
         let tokenData = {_id:user._id,username:user.username,type:user.type}
 
-        const token = await UserService.generateToken(tokenData, "secretKey", "1d")
+        const token = await UserService.generateToken(tokenData, "secretKey")
 
         res.status(200).json({status:true, token:token})
     } catch(error) {
