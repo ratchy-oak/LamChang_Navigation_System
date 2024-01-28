eee71482bd994914fa801bb0a66a778f2241253e
Ratchakrit Mojomsin <rachakritmojomsin@gmail.com>
2024-01-28 21:00:51 +0700

    28/01/2567

--- application/lib/config/config.dart
+++ application/lib/config/config.dart
@@ -1,4 +1,5 @@
-const url = 'http://192.168.0.24:3000/';
+const url = 'http://192.168.1.166:3000/';
 const registration = "${url}registration";
 const login = "${url}login";
 const forgetpassword = "${url}forgetpassword";
+const String googleApiKey = "AIzaSyCx880CMVrF9wm8fwocaUOTkw-cRPp-5Yo";
