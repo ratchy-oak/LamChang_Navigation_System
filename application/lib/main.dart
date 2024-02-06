b184540e2b1e8f41aa39a09f07f13a0802c0131d
Ratchakrit Mojomsin <rachakritmojomsin@gmail.com>
2024-02-06 17:27:40 +0700

    06/02/2567

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:application/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:application/styles/app_colors.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
