ab47f5dbe255c8c9192e47b39bd1030033ca135f
Ratchakrit Mojomsin <rachakritmojomsin@gmail.com>
2024-03-18 11:06:59 +0700

    18/03/2567

--- application/lib/config/config.dart
+++ application/lib/config/config.dart
@@ -1,5 +1,4 @@
-const url = 'http://192.168.0.52:3000/';
+const url = 'http://172.18.22.152:3000/';
 const registration = "${url}registration";
 const login = "${url}login";
 const forgetpassword = "${url}forgetpassword";
-const String googleAPiKey = "AIzaSyCx880CMVrF9wm8fwocaUOTkw-cRPp-5Yo";
