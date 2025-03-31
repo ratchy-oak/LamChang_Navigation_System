4decd18a319d5a4533ac63d06d823e9f1c97bc76
Ratchakrit Mojomsin <rachakritmojomsin@gmail.com>
2025-03-31 23:22:32 +0700

    31/03/2025

--- application/ios/Runner/AppDelegate.swift
+++ application/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCx880CMVrF9wm8fwocaUOTkw-cRPp-5Yo")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
